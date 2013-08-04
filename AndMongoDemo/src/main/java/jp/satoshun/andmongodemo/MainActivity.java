d7b8a1be6ce238070c3086ce76696c424e0993aa
satoshun <satoshun@inblue.co.jp>
2013-08-04 14:58:58 +0900

    andmongo ok

--- AndMongoDemo/src/main/java/jp/satoshun/andmongodemo/MainActivity.java
+++ AndMongoDemo/src/main/java/jp/satoshun/andmongodemo/MainActivity.java
@@ -1,8 +1,10 @@
 package jp.satoshun.andmongodemo;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
+
+import jp.co.satoshun.andmongo.MongoClient;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +12,8 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        MongoClient client;
     }
 
     @Override
